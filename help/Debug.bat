ae97eb59198f3d2210db838daf914d9ee8217847
Cracker1000 <ashishpurohit271@gmail.com>
2023-09-03 00:08:54 +0530

    Fix element files

--- help/Debug.bat
+++ help/Debug.bat
@@ -1,6 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat""
-cd /d C:\The-Powder-Toy&meson build-debug&cd build-debug&ninja
-cmd /k
-
-
-
+cd /d C:\The-Powder-Toy
+meson setup build-debug
+cd build-debug
+meson compile
