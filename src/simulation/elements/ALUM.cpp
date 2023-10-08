299b2334deac601d568d18bbc24a48f1edc82584
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2023-10-08 16:03:57 -0400

    Added basic properties and stole TTAN blocking

--- src/simulation/elements/ALUM.cpp
+++ src/simulation/elements/ALUM.cpp
@@ -1,12 +1,15 @@
 #include "simulation/ElementCommon.h"
+#include "simulation/Air.h"
+
+static int update(UPDATE_FUNC_ARGS);
 
 void Element::Element_ALUM()
 {
 	Identifier = "DEFAULT_PT_ALUM";
 	Name = "ALUM";
-	Colour = PIXPACK(0x404060);
+	Colour = 0xB0B0C4_rgb;
 	MenuVisible = 1;
-	MenuSection = SC_ELEC;
+	MenuSection = SC_SOLIDS;
 	Enabled = 1;
 
 	Advection = 0.0f;
@@ -22,14 +25,14 @@ void Element::Element_ALUM()
 	Flammable = 0;
 	Explosive = 0;
 	Meltable = 1;
-	Hardness = 1;
+	Hardness = 20;
 
 	Weight = 100;
 
 	HeatConduct = 251;
 	Description = "Aluminium.";
 
-	Properties = TYPE_SOLID|PROP_CONDUCTS|PROP_LIFE_DEC|PROP_HOT_GLOW;
+	Properties = TYPE_SOLID|PROP_CONDUCTS|PROP_LIFE_DEC;
 
 	LowPressure = IPL;
 	LowPressureTransition = NT;
@@ -37,6 +40,38 @@ void Element::Element_ALUM()
 	HighPressureTransition = NT;
 	LowTemperature = ITL;
 	LowTemperatureTransition = NT;
-	HighTemperature = 1273.0f;
+	HighTemperature = 660.32f;
 	HighTemperatureTransition = PT_LAVA;
+
+	Update = &update;
+}
+
+static int update(UPDATE_FUNC_ARGS)
+{
+	int alum = 0;
+	if (nt <= 2)
+		alum = 2;
+	else if (parts[i].tmp)
+		alum = 2;
+	else if (nt <= 6)
+	{
+		for (int rx = -1; rx <= 1; rx++)
+		{
+			for (int ry = -1; ry <= 1; ry++)
+			{
+				if (!rx != !ry)
+				{
+					if (TYP(pmap[y+ry][x+rx]) == PT_ALUM)
+						alum++;
+				}
+			}
+		}
+	}
+
+	if (alum >= 2)
+	{
+		sim->air->bmap_blockair[y/CELL][x/CELL] = 1;
+		sim->air->bmap_blockairh[y/CELL][x/CELL] = 0x8;
+	}
+	return 0;
 }
