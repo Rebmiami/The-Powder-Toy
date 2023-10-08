e8a31ef5c8cb788f41c93edbc1a15464039aa519
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2023-10-08 18:08:12 -0400

    Proof-of-concept ALUM pressure interaction

--- src/simulation/elements/ALUM.cpp
+++ src/simulation/elements/ALUM.cpp
@@ -15,7 +15,7 @@ void Element::Element_ALUM()
 	Advection = 0.0f;
 	AirDrag = 0.00f * CFDS;
 	AirLoss = 0.90f;
-	Loss = 0.00f;
+	Loss = 0.70f;
 	Collision = 0.0f;
 	Gravity = 0.0f;
 	Diffusion = 0.00f;
@@ -51,8 +51,6 @@ static int update(UPDATE_FUNC_ARGS)
 	int alum = 0;
 	if (nt <= 2)
 		alum = 2;
-	else if (parts[i].tmp)
-		alum = 2;
 	else if (nt <= 6)
 	{
 		for (int rx = -1; rx <= 1; rx++)
@@ -67,8 +65,43 @@ static int update(UPDATE_FUNC_ARGS)
 			}
 		}
 	}
-
-	if (alum >= 2)
+    float* pressureValues = new float[9];
+    // float* velocityDot = new float[9];
+    int pi = 0;
+    float pressureAvg = 0;
+    for (int rx = -1; rx <= 1; rx++)
+	{
+		for (int ry = -1; ry <= 1; ry++)
+		{
+            pressureValues[pi] = sim->pv[y/CELL + ry][x/CELL + rx];
+            pressureAvg += pressureValues[pi];
+            
+		    // float avx = sim->vx[y/CELL][x/CELL];
+		    // float avy = sim->vy[y/CELL][x/CELL];
+            // float velDot = (rx * avx + ry * avy) / (sqrt(avx * avx + avy * avy));
+            // if (velDot > 0)
+            // {
+            //     velocityDot[pi] = velDot;
+            // }
+            pi++;
+		}
+	}
+    pressureAvg /= 9;
+    float pressureStdev = 0;
+    // float velocitySum = 0;
+    for (int j = 0; j < 9; j++)
+    {
+        pressureStdev += (pressureValues[j] - pressureAvg) * (pressureValues[j] - pressureAvg);
+        // velocitySum += velocityDot[i];
+    }
+    pressureStdev = sqrt(pressureStdev);
+    if (pressureStdev / alum > 20)
+    {
+        // sim->part_change_type(i, x, y, PT_BREC);
+		parts[i].vx += 0.4f*sim->vx[y/CELL][x/CELL];
+		parts[i].vy += 0.4f*sim->vy[y/CELL][x/CELL];
+    }
+	else if (alum >= 2)
 	{
 		sim->air->bmap_blockair[y/CELL][x/CELL] = 1;
 		sim->air->bmap_blockairh[y/CELL][x/CELL] = 0x8;
