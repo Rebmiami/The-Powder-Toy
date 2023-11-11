9af46b6302e9fc72dd0231480b544d2309719192
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2023-11-11 03:36:20 -0500

    Improved ALUM mechanical failure

--- src/simulation/elements/ALUM.cpp
+++ src/simulation/elements/ALUM.cpp
@@ -48,27 +48,31 @@ void Element::Element_ALUM()
 
 static int update(UPDATE_FUNC_ARGS)
 {
-	int alum = 0;
-	if (nt <= 2)
-		alum = 2;
-	else if (nt <= 6)
+	int alum = 0; // Number of orthogonally adjacent aluminium particles, including self.
+	for (int rx = -1; rx <= 1; rx++)
 	{
-		for (int rx = -1; rx <= 1; rx++)
+		for (int ry = -1; ry <= 1; ry++)
 		{
-			for (int ry = -1; ry <= 1; ry++)
+			// Excludes the diagonal neighbors from being detected.
+			if (!rx != !ry)
 			{
-				if (!rx != !ry)
+				if (TYP(pmap[y+ry][x+rx]) == PT_ALUM)
+					alum++;
+				if (alum == 4)
 				{
-					if (TYP(pmap[y+ry][x+rx]) == PT_ALUM)
-						alum++;
+					// Increases relative strength of surface aluminium, which improves the effect of mechanical failure.
+					alum = 3;
+					break;
 				}
 			}
 		}
 	}
-    float* pressureValues = new float[9];
-    // float* velocityDot = new float[9];
-    int pi = 0;
-    float pressureAvg = 0;
+    float pressureValues[9];
+    float velocityDot[9];
+    float velocityStrength[9];
+    int pi = 0; // Not the numerical constant, short for "pressure index"
+    float pressureAvg = 0; // The average pressure level around this particle.
+    float velocityAvg = 0; // The average air speed around this particle.
     for (int rx = -1; rx <= 1; rx++)
 	{
 		for (int ry = -1; ry <= 1; ry++)
@@ -76,35 +80,50 @@ static int update(UPDATE_FUNC_ARGS)
             pressureValues[pi] = sim->pv[y/CELL + ry][x/CELL + rx];
             pressureAvg += pressureValues[pi];
             
-		    // float avx = sim->vx[y/CELL][x/CELL];
-		    // float avy = sim->vy[y/CELL][x/CELL];
-            // float velDot = (rx * avx + ry * avy) / (sqrt(avx * avx + avy * avy));
-            // if (velDot > 0)
-            // {
-            //     velocityDot[pi] = velDot;
-            // }
+		    float avx = sim->vx[y/CELL + ry][x/CELL + rx];
+		    float avy = sim->vy[y/CELL + ry][x/CELL + rx];
+            velocityStrength[pi] = sqrt(avx * avx + avy * avy);
+			velocityAvg += velocityStrength[pi];
+            velocityDot[pi] = 0;
+            if (velocityStrength[pi] > 0.1f)
+            {
+				// Calculate the dot product of air velocity based on its relative position to this particle.
+				// Positive = flowing towards the particle, negative = flowing away from the particle
+            	float velDot = (rx * avx + ry * avy) / velocityStrength[pi];
+                velocityDot[pi] = velDot;
+            }
             pi++;
 		}
 	}
     pressureAvg /= 9;
-    float pressureStdev = 0;
-    // float velocitySum = 0;
+    float pressureStdev = 0; // The intensity of the pressure gradient surrounding this particle.
+    float velocitySum = 0; // The amount of direct force this particle is receiving from velocity.
     for (int j = 0; j < 9; j++)
     {
         pressureStdev += (pressureValues[j] - pressureAvg) * (pressureValues[j] - pressureAvg);
-        // velocitySum += velocityDot[i];
+        velocitySum += velocityDot[j];
     }
     pressureStdev = sqrt(pressureStdev);
-    if (pressureStdev / alum > 20)
+	// Aluminium can withstand high pressure and high velocity, but struggles with both at once.
+	// Additionally, it is much stronger in bulk than in thin sheets.
+	// This has the effect of failures in one area of a pressurized container causing the entire thing to violently burst open.
+	float strain = (pressureStdev + velocitySum + pressureStdev * velocityAvg * 0.05f) / (alum + 1);
+    
+	if (strain > 20) // Deform under immense stress from pressure
     {
-        // sim->part_change_type(i, x, y, PT_BREC);
 		parts[i].vx += 0.4f*sim->vx[y/CELL][x/CELL];
 		parts[i].vy += 0.4f*sim->vy[y/CELL][x/CELL];
     }
-	else if (alum >= 2)
+	else if (alum >= 2) // Block pressure when not under too much stress
 	{
 		sim->air->bmap_blockair[y/CELL][x/CELL] = 1;
 		sim->air->bmap_blockairh[y/CELL][x/CELL] = 0x8;
+	}
+	// When particles are broken off of a mass of aluminium and exposed to high air velocity, break into powder.
+	if (nt == 8 && (pressureStdev + velocityAvg) / (alum + 1) > 40)
+	{
+		// Placeholder. Aluminium dust will be added as a new element later.
+		sim->part_change_type(i, x, y, PT_BREC);
 	}
 	return 0;
 }
